7a06ad20cee1035f517cca169a96310114cff192
Dhruvil Gajjar <dgajjar999@gmail.com>
2020-07-30 22:20:51 +0530

    add card component

--- components/AppText.jsx
+++ components/AppText.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Text, StyleSheet, Platform } from "react-native";
 
 function AppText({ children }) {
-  return <Text style={styles.text}>{children}</Text>;
+  return <Text style={[styles.text, style]}>{children}</Text>;
 }
 
 const styles = StyleSheet.create({
---
+++ components/Card.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { StyleSheet, Text, View, Image } from "react-native";
+
+import colors from "../config/colors";
+import AppText from "./AppText";
+
+function Card({ title, subTitle, image }) {
+  return (
+    <View style={styles.card}>
+      <Image source={image} style={styles.image} />
+      <View style={styles.detailsContainer}>
+        <AppText style={styles.title}>{title}</AppText>
+        <AppText style={styles.subTitle}>{subTitle}</AppText>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  card: {
+    borderRadius: 25,
+    backgroundColor: colors.white,
+    marginBottom: 20,
+    overflow: "hidden",
+  },
+
+  image: {
+    width: "100%",
+    height: 200,
+  },
+
+  detailsContainer: {
+    padding: 20,
+  },
+
+  title: {
+    marginBottom: 7,
+  },
+
+  subTitle: {
+    color: colors.secondary,
+    fontWeight: "bold",
+  },
+});
+
+export default Card;
