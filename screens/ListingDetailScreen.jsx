ce5daa44adb9ab991d12b155c83808df572fb866
Dhruvil Gajjar <dgajjar999@gmail.com>
2020-07-31 21:36:23 +0530

    add style on list item component

--- screens/ListingDetailScreen.jsx
+++ screens/ListingDetailScreen.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { StyleSheet, Text, View, Image } from "react-native";
 
 import AppText from "../components/AppText";
+import ListItem from "../components/ListItem";
 import colors from "../config/colors";
 
 function ListingDetailScreen() {
@@ -11,6 +12,13 @@ function ListingDetailScreen() {
       <View style={styles.detailsContainer}>
         <AppText style={styles.title}>Red jecket for sale</AppText>
         <AppText style={styles.price}>$100</AppText>
+        <View style={styles.userContainer}>
+          <ListItem
+            image={require("../assets/mosh.jpg")}
+            title='Mosh Hamedani'
+            subTitle='5 Listing'
+          />
+        </View>
       </View>
     </View>
   );
@@ -36,6 +44,10 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
     fontSize: 20,
     marginVertical: 10,
+  },
+
+  userContainer: {
+    marginVertical: 40,
   },
 });
 
