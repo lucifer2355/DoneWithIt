e85b008e7845fa192221869b50234292e63222b5
Dhruvil Gajjar <dgajjar999@gmail.com>
2020-07-31 21:41:45 +0530

    add icon

--- screens/ViewImageScreen.jsx
+++ screens/ViewImageScreen.jsx
@@ -1,13 +1,22 @@
 import React from "react";
 import { StyleSheet, Text, View, Image } from "react-native";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 import colors from "../config/colors";
 
 function ViewImageScreen() {
   return (
     <View style={styles.container}>
-      <View style={styles.closeIcon}></View>
-      <View style={styles.deleteIcon}></View>
+      <View style={styles.closeIcon}>
+        <MaterialCommunityIcons name='close' color='white' size={30} />
+      </View>
+      <View style={styles.deleteIcon}>
+        <MaterialCommunityIcons
+          name='trash-can-outline'
+          color='white'
+          size={30}
+        />
+      </View>
       <Image
         resizeMode='contain'
         source={require("../assets/chair.jpg")}
@@ -24,18 +33,12 @@ const styles = StyleSheet.create({
   },
 
   closeIcon: {
-    width: 50,
-    height: 50,
-    backgroundColor: colors.primary,
     position: "absolute",
     top: 40,
     left: 30,
   },
 
   deleteIcon: {
-    width: 50,
-    height: 50,
-    backgroundColor: colors.secondary,
     position: "absolute",
     top: 40,
     right: 30,
