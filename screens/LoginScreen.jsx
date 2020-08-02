51b54c5d75dd5e80495750af1c939060c21b8ad1
Dhruvil Gajjar <dgajjar999@gmail.com>
2020-08-02 19:49:43 +0530

    add formik

--- screens/LoginScreen.jsx
+++ screens/LoginScreen.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { StyleSheet, Text, View, Image } from "react-native";
+import { Formik } from "formik";
 
 import Screen from "../components/Screen";
 import AppTextInput from "../components/AppTextInput";
