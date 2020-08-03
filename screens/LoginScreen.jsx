5294c8acdf68c90568e637d12a80e9bb641ff594
Dhruvil Gajjar <dgajjar999@gmail.com>
2020-08-03 22:00:59 +0530

    add index file in forms folder

--- screens/LoginScreen.jsx
+++ screens/LoginScreen.jsx
@@ -1,11 +1,9 @@
 import React from "react";
-import { StyleSheet, Text, View, Image } from "react-native";
+import { StyleSheet, Image } from "react-native";
 import * as Yup from "yup";
 
 import Screen from "../components/Screen";
-import AppFormField from "../components/forms/AppFormField";
-import SubmitButton from "../components/forms/SubmitButton";
-import AppForm from "../components/forms/AppForm";
+import { AppForm, AppFormField, SubmitButton } from "../components/forms";
 
 const validationSchema = Yup.object().shape({
   email: Yup.string().required().email().label("Email"),
