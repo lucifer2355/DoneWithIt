4046b5a4ee94874beb4c4b224ac5257574c09724
lucifer2355 <dgjjar999@gmail.com>
2020-08-06 22:10:14 +0530

    chnage

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { StyleSheet, Text, View, SafeAreaView } from "react-native";
+import { StyleSheet } from "react-native";
 import { useDimensions } from "@react-native-community/hooks";
 import * as Permissions from "expo-permissions";
 import * as ImagePicker from "expo-image-picker";
