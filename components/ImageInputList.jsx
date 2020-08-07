fa0b66623cb41c7e2f92edf756d101bd53f84e70
lucifer2355 <dgajjar999@gmail.com>
2020-08-07 21:27:37 +0530

    add style to image input list

--- components/ImageInputList.jsx
+++ components/ImageInputList.jsx
@@ -6,11 +6,9 @@ const ImageInputList = ({ imageUris = [], onRemoveImage, onAddImage }) => {
   return (
     <View style={styles.container}>
       {imageUris.map((uri) => (
-        <ImageInput
-          key={uri}
-          imageUri={uri}
-          onChangeImage={() => onRemoveImage(uri)}
-        />
+        <View key={uri} style={styles.image}>
+          <ImageInput imageUri={uri} onChangeImage={() => onRemoveImage(uri)} />
+        </View>
       ))}
       <ImageInput onChangeImage={(uri) => onAddImage(uri)} />
     </View>
@@ -20,6 +18,10 @@ const ImageInputList = ({ imageUris = [], onRemoveImage, onAddImage }) => {
 const styles = StyleSheet.create({
   container: {
     flexDirection: "row",
+  },
+
+  image: {
+    marginRight: 10,
   },
 });
 
