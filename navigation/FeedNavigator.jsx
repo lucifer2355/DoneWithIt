774bf00ab40434404ad931cf991f52e6ef670653
lucifer2355 <dgajjar999@gmail.com>
2020-08-08 15:24:54 +0530

    add modal mode for new screen transaction effect

--- navigation/FeedNavigator.jsx
+++ navigation/FeedNavigator.jsx
@@ -7,9 +7,13 @@ import ListingDetailScreen from "../screens/ListingDetailScreen";
 const Stack = createStackNavigator();
 
 const FeedNavigator = () => (
-  <Stack.Navigator>
+  <Stack.Navigator mode='modal'>
     <Stack.Screen name='Listings' component={ListingScreen} />
-    <Stack.Screen name='ListingDetails' component={ListingDetailScreen} />
+    <Stack.Screen
+      name='ListingDetails'
+      component={ListingDetailScreen}
+      options={{ headerShown: false }}
+    />
   </Stack.Navigator>
 );
 
