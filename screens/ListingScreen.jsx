8a71577b228c5973ba7f80af6b6feca31d69ae4f
lucifer2355 <dgajjar999@gmail.com>
2020-08-10 21:46:22 +0530

    add activity indicator

--- screens/ListingScreen.jsx
+++ screens/ListingScreen.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { StyleSheet, FlatList } from "react-native";
+import { StyleSheet, FlatList, ActivityIndicator } from "react-native";
 
 import Screen from "../components/Screen";
 import routes from "../navigation/routes";
@@ -12,13 +12,17 @@ import AppButton from "../components/AppButton";
 const ListingScreen = ({ navigation }) => {
   const [listings, setListings] = useState([]);
   const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     loadListings();
   }, []);
 
   const loadListings = async () => {
+    setLoading(true);
     const response = await listingsApi.getListings();
+    setLoading(false);
+
     if (!response.ok) return setError(true);
 
     setError(false);
@@ -33,6 +37,7 @@ const ListingScreen = ({ navigation }) => {
           <AppButton title='Retry' onPress={loadListings} />
         </>
       )}
+      <ActivityIndicator animating={loading} size='large' />
       <FlatList
         data={listings}
         keyExtractor={(item) => item.id.toString()}
