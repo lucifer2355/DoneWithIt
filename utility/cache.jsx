bd8c2f3b21287d8562b96adf240c32f53bd1c6f0
lucifer2355 <dgajjar999@gmail.com>
2020-08-13 21:28:31 +0530

    add async storage

---
+++ utility/cache.jsx
@@ -0,0 +1,44 @@
+import { AsyncStorage } from "react-native";
+import moment from "moment";
+
+const prefix = "cache";
+const expiryInMinutes = 5;
+
+const store = async (key, value) => {
+  try {
+    const item = {
+      value,
+      timestamp: Date.now(),
+    };
+
+    await AsyncStorage.setItem(prefix + key, JSON.stringify(item));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const isExpired = (item) => {
+  const now = moment(Date.now());
+  const storedTime = moment(item.timestamp);
+  return now.diff(storedTime, "minute") > expiryInMinutes;
+};
+
+const get = async (key) => {
+  try {
+    const value = await AsyncStorage.getItem(prefix + key);
+    const item = JSON.stringify(value);
+
+    if (!item) return null;
+
+    if (isExpired(item)) {
+      await AsyncStorage.removeItem(prefix + key);
+      return null;
+    }
+
+    return item.value;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export default { store };
